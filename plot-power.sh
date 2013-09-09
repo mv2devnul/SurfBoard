d06450889eb915880630245a2248038f9b564755
Mark VandenBrink <mark@vandenbrink.us>
2013-09-09 16:46:20 -0500

    fix cut/paste error

--- plot-power.sh
+++ plot-power.sh
@@ -1,13 +1,14 @@
 if [ $# -ne 2 ]; then
     echo "usage $0 input-file output-file"
+	exit 1
 fi
 if [ -f "$2" ]; then
     echo "output file $2 exists, please delete it or use another file name for output"
-    exit 1
+    exit 2
 fi
 if [ -f "$2.svg" ]; then
     echo "SVG output file $2.svg exists, please delete it or use another file name for output"
-    exit 2
+    exit 3
 fi
 
 ccl64 -e '(quicklisp:quickload "surfboard")' -e "(report-levels \"$1\" \"$2\")" -e "(quit)"
