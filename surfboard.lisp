5544a8e984d628cb19f5460f4275378e7e3e7cac
Mark VandenBrink <mark@vandenbrink.us>
2013-09-10 14:25:24 -0500

    added empty record when we can't get to modem; also fixed bug in get-timestamp-string

--- surfboard.lisp
+++ surfboard.lisp
@@ -1,9 +1,38 @@
 ;;; -*- Mode: Lisp;  show-trailing-whitespace: t; Base: 10; indent-tabs: nil; Syntax: ANSI-Common-Lisp; Package: CL-USER; -*-
 ;;; Copyright (c) 2013, Mark VandenBrink. All rights reserved.
 
-(defparameter *modem-status* "http://192.168.100.1/indexData.htm")
-(defparameter *modem-stats*  "http://192.168.100.1/cmSignalData.htm")
-(defparameter *modem-logs*   "http://192.168.100.1/cmLogsData.htm")
+(defparameter *modem-status-uri* "http://192.168.100.1/indexData.htm")
+(defparameter *modem-stats-uri*  "http://192.168.100.1/cmSignalData.htm")
+(defparameter *modem-logs-uri*   "http://192.168.100.1/cmLogsData.htm")
+
+(defparameter *modem-status-headers*
+  '("DOCSIS Downstream Channel Acquisition"
+    "DOCSIS Ranging"
+    "Establish IP Connectivity using DHCP"
+    "Establish Time Of Day"
+    "Transfer Operational Parameters through TFTP"
+    "Register Connection"
+    "Cable Modem Status"
+    "Initialize Baseline Privacy"
+    "Current Time and Date"
+    "System Up Time"))
+
+(defparameter *modem-stats-headers*
+  '("Downstream Channel ID"
+    "Downstream Frequency"
+    "Downstream Signal to Noise Ratio"
+    "Downstream Downstream Modulation"
+    "Downstream Power Level"
+    "Upstream Channel ID"
+    "Upstream Frequency"
+    "Upstream Ranging Service ID"
+    "Upstream Symbol Rate"
+    "Upstream Power Level"
+    "Upstream Ranging Status"
+    "Signal Stats Channel ID"
+    "Signal Stats Total Unerrored Codewords"
+    "Signal Stats Total Correctable Codewords"
+    "Signal Stats Total Uncorrectable Codewords"))
 
 (defstruct table-row name value)
 
@@ -15,16 +44,23 @@
                    (push (make-table-row :name name :value value) this-run))
                  ((list* (satisfies keywordp) _ children)
                   (map nil #'recurse children)))))
-      (recurse (closure-html:parse (drakma:http-request *modem-status*) (closure-html:make-lhtml-builder))))
+      (recurse (closure-html:parse (drakma:http-request *modem-status-uri* :connection-timeout 5) (closure-html:make-lhtml-builder))))
     (nreverse this-run)))
 
 (defun upto-char (string c)
   (subseq string 0 (position c string)))
 
+;;; warning: hideous hack to follow. The SB6121 embeds a table inside a taoble, which makes using OPTIMA
+;;; messy.  I really should figure out how to do this all with OPTIMA, but this is just easier...
 (defparameter *pattern* "<TR><TD>Power Level<TABLE border=0 cellPadding=0 cellSpacing=0 width=300>        <TBODY><TR>          <TD align=left><SMALL>The Downstream Power Level reading is a             snapshot taken at the time this page was requested. Please             Reload/Refresh this Page for a new reading           </SMALL></TD></TR></TBODY></TABLE></TD>")
 (defmacro trim-string (s) `(string-trim '(#\Null #\Space #\Newline) ,s))
+(defun get-timestamp-string ()
+  "returns a string with year-month-day-hour-minute-second"
+  (multiple-value-bind (second minute hour date month year) (get-decoded-time)
+    (format nil "~4d-~2,'0d-~2,'0d-~2,'0d-~2,'0d-~2,'0d" year month date hour minute second)))
 
 (defun get-modem-stats ()
+  "Parse the modem signal stats page of the SB6121"
   (let ((this-run)
         (html))
     (labels ((recurse (x)
@@ -37,7 +73,7 @@
                  ((list* (satisfies keywordp) _ children)
                   (map nil #'recurse children)))))
 
-      (setf html (cl-ppcre:regex-replace (cl-ppcre:create-scanner *pattern*) (drakma:http-request *modem-stats*) "<TR><TD>Power Level</TD>"))
+      (setf html (cl-ppcre:regex-replace (cl-ppcre:create-scanner *pattern*) (drakma:http-request *modem-stats-uri* :connection-timeout 5) "<TR><TD>Power Level</TD>"))
       (setf html (cl-ppcre:regex-replace (cl-ppcre:create-scanner "<BR>") html ""))
       (setf html (cl-ppcre:regex-replace (cl-ppcre:create-scanner "&nbsp;") html ""))
       (recurse (closure-html:parse html (closure-html:make-lhtml-builder))))
@@ -46,6 +82,7 @@
 (defstruct log-row time priority code message)
 
 (defun get-modem-log ()
+  "Parse the modem log page of the SB6121"
   (let (this-run)
     (labels ((recurse (x)
                (optima:match x
@@ -53,64 +90,45 @@
                    (push (make-log-row :time col1 :priority col2 :code col3 :message col4) this-run))
                  ((list* (satisfies keywordp) _ children)
                   (map nil #'recurse children)))))
-      (recurse (closure-html:parse (drakma:http-request *modem-logs*) (closure-html:make-lhtml-builder)))
+      (recurse (closure-html:parse (drakma:http-request *modem-logs-uri* :connection-timeout 5) (closure-html:make-lhtml-builder)))
       (nreverse this-run))))
 
-(defun get-timestamp-string ()
-  "returns a string with year-month-day-hour-minute-second"
-  (multiple-value-bind (second minute hour date month year day daylight-p zone) (get-decoded-time)
-    (declare (ignore zone daylight-p date))
-    (format nil "~4d-~2,'0d-~2,'0d-~2,'0d-~2,'0d-~2,'0d" year month day hour minute second)))
 
 (defun write-header-line (stream)
-  (fare-csv:write-csv-line '("Timestamp"
-                             "DOCSIS Downstream Channel Acquisition"
-                             "DOCSIS Ranging"
-                             "Establish IP Connectivity using DHCP"
-                             "Establish Time Of Day"
-                             "Transfer Operational Parameters through TFTP"
-                             "Register Connection"
-                             "Cable Modem Status"
-                             "Initialize Baseline Privacy"
-                             "Current Time and Date"
-                             "System Up Time"
-                             "Downstream Channel ID"
-                             "Downstream Frequency"
-                             "Downstream Signal to Noise Ratio"
-                             "Downstream Downstream Modulation"
-                             "Downstream Power Level"
-                             "Upstream Channel ID"
-                             "Upstream Frequency"
-                             "Upstream Ranging Service ID"
-                             "Upstream Symbol Rate"
-                             "Upstream Power Level"
-                             "Upstream Ranging Status"
-                             "Signal Stats Channel ID"
-                             "Signal Stats Total Unerrored Codewords"
-                             "Signal Stats Total Correctable Codewords"
-                             "Signal Stats Total Uncorrectable Codewords")
-                           stream))
+  "Write the header line for the CSV file."
+  (fare-csv:write-csv-line (append (list "Timestamp") *modem-status-headers* *modem-stats-headers*) stream))
 
-(defun write-table-line (stream modem-status modem-stats)
+(defun write-table-line (stream &optional modem-status modem-stats)
+  "Get the modem status, then stats, and create/write a line to the CSV file."
   (let ((s))
     (push (get-timestamp-string) s)
-    (dolist (e modem-status)
-      (push (table-row-value e) s))
-    (dolist (e modem-stats)
-      (push (table-row-value e) s))
+    (if modem-status
+        (dolist (e modem-status)
+          (push (table-row-value e) s))
+        (dotimes (i (length *modem-status-headers*))
+          (push 0 s)))
+    (if modem-stats
+        (dolist (e modem-stats)
+          (push (table-row-value e) s))
+        (dotimes (i (length *modem-stats-headers*))
+          (push 0 s)))
+
     (fare-csv:write-csv-line (nreverse s) stream)))
 
 (defparameter *last-log* nil)
 
 (defun log-row-equal (a b)
+  "Compare two modem log entries."
   (and (string= (log-row-time a) (log-row-time b))
        (string= (log-row-priority a) (log-row-priority b))
        (string= (log-row-code a) (log-row-code b))
        (string= (log-row-message a) (log-row-message b))))
 
 (defun process-modem-log (log f)
+  "Tries to keep a running log of the modem log; NOT totally correct!!!!"
   (let ((ts (get-timestamp-string)))
     (when (null *last-log*)
+      ;; first time called
       (fare-csv:write-csv-line '(Real-time Time Priority Code Message) f)
       (dolist (l log)
         (fare-csv:write-csv-line (list ts
@@ -122,6 +140,7 @@
       (return-from process-modem-log t))
 
     (when (not (log-row-equal (first log) (first *last-log*)))
+      ;; the log we just read has a different entry for the first record. try to splice together old/new
       (block move-old
         (dolist (l log)
           (if (not (log-row-equal l (first *last-log*)))
@@ -134,29 +153,37 @@
       (setf *last-log* log))))
 
 (defun watch-modem (&optional (log-name "modem") (interval 60))
+  "Sit in an infinite loop, reading the modem status/stats/log at INTERVAL seconds."
   (setf *last-log* nil)
-  (format t "Writing to ~a at ~:d interval~%" log-name interval)
+  (format t "Writing to ~a at ~:d second interval~%" log-name interval)
   (let ((count 0)
         (ml  (open (format nil "~a-log.csv" log-name) :direction :output :if-does-not-exist :create :if-exists :rename))
         (err (open (format nil "~a.errs" log-name)    :direction :output :if-does-not-exist :create :if-exists :rename))
         (f   (open (format nil "~a.csv" log-name)     :direction :output :if-does-not-exist :create :if-exists :rename)))
     (unwind-protect
-         (progn
-           (write-header-line f)
-           (loop
-             (format t "Starting iteration ~:d~%" (incf count))
-             (handler-case
-                 (let* ((modem-log (get-modem-log))
-                        (modem-stats (get-modem-stats))
-                        (modem-status (get-modem-status)))
-                   (write-table-line f modem-status modem-stats)
-                   (process-modem-log modem-log ml)
-                   (finish-output ml)
-                   (finish-output err)
-                   (finish-output f))
-               (condition (c)
-                 (format err "At ~a, got condition: <~a>~%" (get-timestamp-string) c)))
-             (sleep interval)))
+      (fare-csv:with-strict-rfc4180-csv-syntax ()
+        (write-header-line f)
+        (loop
+          (format t "Starting iteration ~:d~%" (incf count))
+          (handler-case
+              (let* ((modem-log (get-modem-log))
+                     (modem-stats (get-modem-stats))
+                     (modem-status (get-modem-status)))
+                (write-table-line f modem-status modem-stats)
+                (process-modem-log modem-log ml))
+            (condition (c)
+              ;; most likely cause for error is modem is down, so write a fake entry
+              ;; (ie, with power-levels 0, etc)
+              (write-table-line f)
+              (let ((line (format nil "At ~a, got condition: <~a>~%" (get-timestamp-string) c)))
+                (format *error-output* "~a~%" line)
+                (format err "~a~%" line))))
+          (finish-output ml)
+          (finish-output err)
+          (finish-output f)
+          (sleep interval)))
+
+      ;; unwind-protect cleanup.
       (when f (finish-output f) (close f))
       (when err (finish-output err) (close err))
       (when ml (finish-output ml) (close ml)))))
