b2dea190740c7ac02cd4095f474f90ddd657840c
Mark VandenBrink <mark@vandenbrink.us>
2013-09-20 11:17:56 -0500

    checkpoint: added code to make sure we have valid strings when we read the modem web pages

--- surfboard.lisp
+++ surfboard.lisp
@@ -9,7 +9,10 @@
 (defparameter *modem-logs-uri*   "http://192.168.100.1/cmLogsData.htm")
 
 #+:CONCURRENT-PLOT
-(defparameter *chanl*            (make-instance 'chanl:bounded-channel))
+(progn
+  (defparameter *chanl* nil)
+  (defmacro c-send (d) `(chanl:send *chanl* ,d))
+  (defmacro c-recv ()  `(chanl:recv *chanl*)))
 
 (defparameter *modem-status-headers*
   '("DOCSIS Downstream Channel Acquisition"
@@ -68,34 +71,42 @@
   signal-stats-total-correctable-codewords
   signal-stats-total-uncorrectable-codewords)
 
-(defun make-modem-record (line)
-  (make-modem-log-record
-   :timestamp (nth 0 line)
-   :docsis-downstream-channel-acquisition (nth 1 line)
-   :docsis-ranging (nth 2 line)
-   :establish-ip-connectivity-using-dhcp (nth 3 line)
-   :establish-time-of-day (nth 4 line)
-   :transfer-operational-parameters-through-tftp (nth 5 line)
-   :register-connection (nth 6 line)
-   :cable-modem-status (nth 7 line)
-   :initialize-baseline-privacy (nth 8 line)
-   :current-time-and-date (nth 9 line)
-   :system-up-time (nth 10 line)
-   :downstream-channel-id (nth 11 line)
-   :downstream-frequency (nth 12 line)
-   :downstream-signal-to-noise-ratio (nth 13 line)
-   :downstream-downstream-modulation (nth 14 line)
-   :downstream-power-level (nth 15 line)
-   :upstream-channel-id (nth 16 line)
-   :upstream-frequency (nth 17 line)
-   :upstream-ranging-service-id (nth 18 line)
-   :upstream-symbol-rate (nth 19 line)
-   :upstream-power-level (nth 20 line)
-   :upstream-ranging-status (nth 21 line)
-   :signal-stats-channel-id (nth 22 line)
-   :signal-stats-total-unerrored-codewords (nth 23 line)
-   :signal-stats-total-correctable-codewords (nth 24 line)
-   :signal-stats-total-uncorrectable-codewords (nth 25 line)))
+(defun ensure-strings (in-lst)
+  (loop for e in in-lst collecting (if (stringp e) e "BAD STRING")))
+
+(defun write-csv-line (lst stream)
+  (fare-csv:with-strict-rfc4180-csv-syntax ()
+    (fare-csv:write-csv-line lst stream)))
+
+(defun make-modem-record (in-line)
+  (let ((line (ensure-strings in-line)))
+    (make-modem-log-record
+     :timestamp (nth 0 line)
+     :docsis-downstream-channel-acquisition (nth 1 line)
+     :docsis-ranging (nth 2 line)
+     :establish-ip-connectivity-using-dhcp (nth 3 line)
+     :establish-time-of-day (nth 4 line)
+     :transfer-operational-parameters-through-tftp (nth 5 line)
+     :register-connection (nth 6 line)
+     :cable-modem-status (nth 7 line)
+     :initialize-baseline-privacy (nth 8 line)
+     :current-time-and-date (nth 9 line)
+     :system-up-time (nth 10 line)
+     :downstream-channel-id (nth 11 line)
+     :downstream-frequency (nth 12 line)
+     :downstream-signal-to-noise-ratio (nth 13 line)
+     :downstream-downstream-modulation (nth 14 line)
+     :downstream-power-level (nth 15 line)
+     :upstream-channel-id (nth 16 line)
+     :upstream-frequency (nth 17 line)
+     :upstream-ranging-service-id (nth 18 line)
+     :upstream-symbol-rate (nth 19 line)
+     :upstream-power-level (nth 20 line)
+     :upstream-ranging-status (nth 21 line)
+     :signal-stats-channel-id (nth 22 line)
+     :signal-stats-total-unerrored-codewords (nth 23 line)
+     :signal-stats-total-correctable-codewords (nth 24 line)
+     :signal-stats-total-uncorrectable-codewords (nth 25 line))))
 
 (defstruct table-row name value)
 
@@ -143,11 +154,13 @@
     (nreverse this-run)))
 
 (defstruct log-row time priority code message)
-(defun make-log-row-record (lst)
-  (make-log-row :time (nth 1 lst)
-                :priority (nth 2 lst)
-                :code (nth 3 lst)
-                :message (nth 4 lst)))
+
+(defun make-log-row-record (in-lst)
+  (let ((lst (ensure-strings in-lst)))
+    (make-log-row :time (nth 1 lst)
+                  :priority (nth 2 lst)
+                  :code (nth 3 lst)
+                  :message (nth 4 lst))))
 
 (defun get-modem-log ()
   "Parse the modem log page of the SB6121"
@@ -159,33 +172,38 @@
                  ((list* (satisfies keywordp) _ children)
                   (map nil #'recurse children)))))
       (recurse (closure-html:parse (drakma:http-request *modem-logs-uri* :connection-timeout 5) (closure-html:make-lhtml-builder)))
-      (nreverse this-run))))
 
+      ;; NB: if we catch the modem during a reboot, it is possible that this webpage wasn't completly finished.
+      ;; If that happens, optima returns a proper list of HTML, which will cause us to crash later in fare-csv writes.
+      ;;
+      (nreverse this-run))))
 
 (defun write-header-line (stream)
   "Write the header line for the CSV file."
-  (fare-csv:with-strict-rfc4180-csv-syntax ()
-    (fare-csv:write-csv-line (append (list "Timestamp") *modem-status-headers* *modem-stats-headers*) stream)))
+  (write-csv-line (append (list "Timestamp") *modem-status-headers* *modem-stats-headers*) stream))
 
 (defun write-table-line (stream &optional modem-status modem-stats)
   "Get the modem status, then stats, and create/write a line to the CSV file."
-  (let ((s))
+  (let (s)
     (push (get-timestamp-string) s)
+
     (if modem-status
         (dolist (e modem-status)
           (push (table-row-value e) s))
         (dotimes (i (length *modem-status-headers*))
           (push 0 s)))
+
     (if modem-stats
         (dolist (e modem-stats)
           (push (table-row-value e) s))
         (dotimes (i (length *modem-stats-headers*))
           (push 0 s)))
 
-    (fare-csv:with-strict-rfc4180-csv-syntax ()
-      (fare-csv:write-csv-line (nreverse s) stream))))
+    (write-csv-line (nreverse s) stream)))
 
-(defparameter *last-log* nil)
+;; modem entries read from the modem are in reverse chronological order
+;; e.g., most recent error is (first *last-log*)
+(defparameter *last-log* nil "list of last log entries  read from http/modem.")
 
 (defun log-row-equal (a b)
   "Compare two modem log entries, ignoring the timestamp we add"
@@ -210,31 +228,32 @@
 NB: the SB 6121 only displays the last 20 error messages, so it is entirely
 possible that we miss capturing errors (e.g. modem gets >20 errors in
 the timeframe from the last time we sampled."
-  (fare-csv:with-strict-rfc4180-csv-syntax ()
-    (let ((ts (get-timestamp-string)))
-      (when (null *last-log*)
-        ;; first time called
-        (fare-csv:write-csv-line '("Timestamp" "Real-time" "Time" "Priority" "Code" "Message") f)
-        (dolist (l log)
-          (fare-csv:write-csv-line (list ts
-                                         (log-row-time l)
-                                         (log-row-priority l)
-                                         (log-row-code l)
-                                         (log-row-message l)) f))
-        (setf *last-log* log)
-        (return-from process-modem-log t))
+  (let ((ts (get-timestamp-string)))
+    (when (null *last-log*)
+      ;; first time called
+      (write-csv-line '("Timestamp" "Real-time" "Time" "Priority" "Code" "Message") f)
+      (dolist (l (nreverse log))
+        (write-csv-line (list ts
+                              (log-row-time l)
+                              (log-row-priority l)
+                              (log-row-code l)
+                              (log-row-message l)) f))
+      (setf *last-log* log)
+      (return-from process-modem-log t))
 
-      (let ((index 0))
-        (while (not (log-row-equal (nth index log) (first *last-log*)))
-          (fare-csv:write-csv-line (list ts
-                                         (log-row-time (nth index log))
-                                         (log-row-priority (nth index log))
-                                         (log-row-code (nth index log))
-                                         (log-row-message (nth index log)))
-                                   f)
-          (incf index)))
-
-      (setf *last-log* log))))
+    (let (new)
+      (loop for row in log do
+        (when (log-row-equal row (first *last-log*))
+          (return))
+        (push row new))
+      (dolist (row (nreverse new))
+        (write-csv-line (list ts
+                              (log-row-time row)
+                              (log-row-priority row)
+                              (log-row-code row)
+                              (log-row-message row))
+                        f)))
+    (setf *last-log* log)))
 
 (defmacro modem-stats-name  (f) `(format nil "~a.csv" ,f))
 (defmacro modem-error-log-name  (f) `(format nil "~a-log.csv" ,f))
@@ -243,55 +262,61 @@ the timeframe from the last time we sampled."
 (defun watch-modem (log-name &optional (interval 60))
   "Sit in an infinite loop, reading the modem status/stats/log at INTERVAL seconds and writing to file-based logs"
   (setf *last-log* nil)
+  (setf *chanl* (make-instance 'chanl:bounded-channel))
   (format t "Writing to ~a at ~:d second interval~%" log-name interval)
 
   (let* ((count                       0)
          (modem-stats-file-exists     (probe-file (modem-stats-name log-name)))
-         (modem-error-log-file        (open (modem-error-log-name  log-name) :direction :output :if-does-not-exist :create :if-exists :append))
+         (modem-error-log-file-exists (probe-file (modem-error-log-name log-name)))
+         (modem-error-log-file        (open (modem-error-log-name log-name) :direction :output :if-does-not-exist :create :if-exists :append))
          (socket-error-file           (open (socket-error-name log-name) :direction :output :if-does-not-exist :create :if-exists :append))
          (modem-stats-file            (open (modem-stats-name  log-name) :direction :output :if-does-not-exist :create :if-exists :append)))
 
     #+:CONCURRENT-PLOT
     (progn
       (chanl:pcall #'chanl-worker-thread)
-      (chanl:send *chanl* "start")
-      (chanl:send *chanl* (modem-stats-name log-name)))
+      (c-send "start")
+      (c-send (modem-stats-name log-name)))
+
+    (when modem-error-log-file-exists
+      ;; since we store the modem log in chronological order (e.g. the last line of file is
+      ;; the most-recent error) we need to reverse what we read
+      (setf *last-log* (nreverse (last (read-modem-log modem-error-log-file) 20))))
 
     (unwind-protect
-      (fare-csv:with-strict-rfc4180-csv-syntax ()
-        (unless modem-stats-file-exists (write-header-line modem-stats-file))
+         (unless modem-stats-file-exists (write-header-line modem-stats-file))
 
         (loop
           (format t "Starting iteration ~:d~%" (incf count))
+
           (handler-case
               (let* ((modem-log (get-modem-log))
                      (modem-stats (get-modem-stats))
                      (modem-status (get-modem-status)))
                 (write-table-line modem-stats-file modem-status modem-stats)
                 (process-modem-log modem-log modem-error-log-file))
-            (condition (c)
-              ;; most likely cause for error is modem is down or responding slowly due to errors, so write a fake entry
-              ;; (ie, with power-levels 0, etc)
+            (usocket:socket-error (c)
               (write-table-line modem-stats-file)
               (let ((line (format nil "At ~a, got condition: <~a>" (get-timestamp-string) c)))
                 (format *error-output* "~a~%" line)
                 (format socket-error-file "~a~%" line))))
+
           (finish-output modem-error-log-file)
           (finish-output socket-error-file)
           (finish-output modem-stats-file)
 
           #+:CONCURRENT-PLOT
-          (chanl:send *chanl* "plot")
+          (c-send "plot")
 
           (sleep interval)))
 
       ;; unwind-protect cleanup.
       #+:CONNCURRENT-PLOT
-      (chanl:send *chanl* "quit")
+      (c-send "quit")
 
       (when modem-stats-file (finish-output modem-stats-file) (close modem-stats-file))
       (when socket-error-file (finish-output socket-error-file) (close socket-error-file))
-      (when modem-error-log-file (finish-output modem-error-log-file) (close modem-error-log-file)))))
+      (when modem-error-log-file (finish-output modem-error-log-file) (close modem-error-log-file))))
 
 #+:CONCURRENT-PLOT
 (let* ((proc)
@@ -350,6 +375,7 @@ the timeframe from the last time we sampled."
     (let ((count 0)
           (outages 0))
       (with-open-file (f file)
+        (read-line f nil) ; throw away header
         (do ((line (read-line f nil) (read-line f nil)))
             ((null line))
           (incf count)
@@ -364,7 +390,7 @@ the timeframe from the last time we sampled."
   (defun chanl-worker-thread ()
     "A thread that sits and waits on *CHANL* and when it receives a msg on *CHANL*, performs the appropriate action."
     (loop
-      (let ((msg (chanl:recv *chanl*)))
+      (let ((msg (c-recv)))
         ;(format *error-output* "Recv: ~a~%" msg)
         (cond ((string= "start" msg)
                (start-gnuplot))
