24f51e3b3ea05cf2b27fcb3ec29e454c066cd504
Mark VandenBrink <mark@vandenbrink.us>
2013-09-24 14:11:44 -0500

    cleanup

--- surfboard.lisp
+++ surfboard.lisp
@@ -1,8 +1,31 @@
 ;;; -*- Mode: Lisp;  show-trailing-whitespace: t; Base: 10; indent-tabs: nil; Syntax: ANSI-Common-Lisp; Package: CL-USER; -*-
 ;;; Copyright (c) 2013, Mark VandenBrink. All rights reserved.
+(in-package #:surfboard)
 
 (eval-when (:load-toplevel :compile-toplevel :execute)
-  (pushnew :CONCURRENT-PLOT *features*))
+  (pushnew :CONCURRENT-PLOT *features*)
+
+  (defun mkstr (&rest args)
+    "Given ARGS, create a string. Case of symbols in ARGS is determined by *PRINT-CASE*.
+Strings' case are as passed in
+eg:
+    (dolist (*print-case* '(:upcase :downcase :capitalize :studly))
+      (format t \"~a -->> ~a~%\" *print-case* (mkstr 'this '-is- \"SPARTA\")))
+yields:
+    UPCASE -->> THIS-IS-SPARTA
+    downcase -->> this-is-SPARTA
+    Capitalize -->> This-Is-SPARTA
+    STUDlY -->> ThiS-IS-SPARTA"
+    (format nil "~{~a~}" args))
+
+  (defun mksym (&rest args)
+    "Intern a symbol in current package.  NB: we coerce to all upcase."
+    (intern (string-upcase (apply #'mkstr args))))
+
+  (defun mkkeyword (&rest args)
+    "Intern a keyword."
+    (intern (string-upcase (apply #'mkstr args)) :keyword))
+  )
 
 (defparameter *modem-status-uri* "http://192.168.100.1/indexData.htm")
 (defparameter *modem-stats-uri*  "http://192.168.100.1/cmSignalData.htm")
@@ -43,7 +66,41 @@
     "Signal Stats Total Correctable Codewords"
     "Signal Stats Total Uncorrectable Codewords"))
 
-(defstruct modem-log-record
+(defun upto-char (string c)
+  "return the part of STRING up until we find the char C"
+  (subseq string 0 (position c string)))
+
+(defun ensure-strings (in-lst)
+  "Given a list, make sure that all members are strings"
+  (loop for e in in-lst collecting (if (stringp e) e "BAD STRING")))
+
+(defmacro trim-string (s)
+  "Given a string S, get rid of spaces, nulls, and newlines from the end."
+  `(string-trim '(#\Null #\Space #\Newline) ,s))
+
+(defun get-timestamp-string ()
+  "returns a string with year-month-day-hour-minute-second"
+  (multiple-value-bind (second minute hour date month year) (get-decoded-time)
+    (format nil "~4d-~2,'0d-~2,'0d-~2,'0d-~2,'0d-~2,'0d" year month date hour minute second)))
+
+(defmacro defstruct+ (name &rest slots)
+  "macro to create a structure and a function to populate that structure from a list.
+The list is expected to match the order of the slots as given to this macro.
+Example: (destruct my-struct slot-1 slot-2 slot3)
+creates my-struct and also a function mk-my-struct that takes a list."
+  `(progn
+     (defstruct ,name ,@(mapcar (lambda (s) s) slots))
+     (defun ,(mksym "mk-" name) (lst)
+       (funcall ',(mksym "make-" name)
+                ,@(loop for i = 0 then (incf i)
+                        for c in slots
+                        collecting (mkkeyword c)
+                        collecting `(nth ,i lst))))))
+
+(defstruct+ modem-status-row name value)                ; for the modem status table
+(defstruct+ modem-log-row time priority code message)   ; for the modem log table
+
+(defstruct+ modem-stats-row                             ; for the modem stats tables
   timestamp
   docsis-downstream-channel-acquisition
   docsis-ranging
@@ -71,67 +128,27 @@
   signal-stats-total-correctable-codewords
   signal-stats-total-uncorrectable-codewords)
 
-(defun ensure-strings (in-lst)
-  (loop for e in in-lst collecting (if (stringp e) e "BAD STRING")))
 
 (defun write-csv-line (lst stream)
+  "Write a CSV line to STREAM constructed from LST."
   (fare-csv:with-strict-rfc4180-csv-syntax ()
     (fare-csv:write-csv-line lst stream)))
 
-(defun make-modem-record (in-line)
-  (let ((line (ensure-strings in-line)))
-    (make-modem-log-record
-     :timestamp (nth 0 line)
-     :docsis-downstream-channel-acquisition (nth 1 line)
-     :docsis-ranging (nth 2 line)
-     :establish-ip-connectivity-using-dhcp (nth 3 line)
-     :establish-time-of-day (nth 4 line)
-     :transfer-operational-parameters-through-tftp (nth 5 line)
-     :register-connection (nth 6 line)
-     :cable-modem-status (nth 7 line)
-     :initialize-baseline-privacy (nth 8 line)
-     :current-time-and-date (nth 9 line)
-     :system-up-time (nth 10 line)
-     :downstream-channel-id (nth 11 line)
-     :downstream-frequency (nth 12 line)
-     :downstream-signal-to-noise-ratio (nth 13 line)
-     :downstream-downstream-modulation (nth 14 line)
-     :downstream-power-level (nth 15 line)
-     :upstream-channel-id (nth 16 line)
-     :upstream-frequency (nth 17 line)
-     :upstream-ranging-service-id (nth 18 line)
-     :upstream-symbol-rate (nth 19 line)
-     :upstream-power-level (nth 20 line)
-     :upstream-ranging-status (nth 21 line)
-     :signal-stats-channel-id (nth 22 line)
-     :signal-stats-total-unerrored-codewords (nth 23 line)
-     :signal-stats-total-correctable-codewords (nth 24 line)
-     :signal-stats-total-uncorrectable-codewords (nth 25 line))))
-
-(defstruct table-row name value)
-
 (defun get-modem-status ()
+  "Get the modem status from the web page."
   (let (this-run)
     (labels ((recurse (x)
                (optima:match x
                  (`(:tr nil (:td nil ,name) (:td nil ,value))
-                   (push (make-table-row :name name :value value) this-run))
+                   (push (mk-modem-status-row (ensure-strings (list name value))) this-run))
                  ((list* (satisfies keywordp) _ children)
                   (map nil #'recurse children)))))
       (recurse (closure-html:parse (drakma:http-request *modem-status-uri* :connection-timeout 5) (closure-html:make-lhtml-builder))))
     (nreverse this-run)))
 
-(defun upto-char (string c)
-  (subseq string 0 (position c string)))
-
-;;; warning: hideous hack to follow. The SB6121 embeds a table inside a taoble, which makes using OPTIMA
+;;; warning: hideous hack to follow. The SB6121 embeds a table inside a table, which makes using OPTIMA
 ;;; messy.  I really should figure out how to do this all with OPTIMA, but this is just easier...
 (defparameter *pattern* "<TR><TD>Power Level<TABLE border=0 cellPadding=0 cellSpacing=0 width=300>        <TBODY><TR>          <TD align=left><SMALL>The Downstream Power Level reading is a             snapshot taken at the time this page was requested. Please             Reload/Refresh this Page for a new reading           </SMALL></TD></TR></TBODY></TABLE></TD>")
-(defmacro trim-string (s) `(string-trim '(#\Null #\Space #\Newline) ,s))
-(defun get-timestamp-string ()
-  "returns a string with year-month-day-hour-minute-second"
-  (multiple-value-bind (second minute hour date month year) (get-decoded-time)
-    (format nil "~4d-~2,'0d-~2,'0d-~2,'0d-~2,'0d-~2,'0d" year month date hour minute second)))
 
 (defun get-modem-stats ()
   "Parse the modem signal stats page of the SB6121"
@@ -142,7 +159,7 @@
                  (`(:tr nil (:td nil ,name) (:td nil ,value))
                    (progn
                      (setf value (trim-string (upto-char value #\No-Break_Space)))
-                     (push (make-table-row :name name :value value) this-run)))
+                     (push (mk-modem-stats-row (ensure-strings (list name value))) this-run)))
 
                  ((list* (satisfies keywordp) _ children)
                   (map nil #'recurse children)))))
@@ -153,29 +170,17 @@
       (recurse (closure-html:parse html (closure-html:make-lhtml-builder))))
     (nreverse this-run)))
 
-(defstruct log-row time priority code message)
-
-(defun make-log-row-record (in-lst)
-  (let ((lst (ensure-strings in-lst)))
-    (make-log-row :time (nth 1 lst)
-                  :priority (nth 2 lst)
-                  :code (nth 3 lst)
-                  :message (nth 4 lst))))
-
 (defun get-modem-log ()
   "Parse the modem log page of the SB6121"
   (let (this-run)
     (labels ((recurse (x)
                (optima:match x
                  (`(:tr nil (:td nil ,col1) (:td nil ,col2) (:td nil ,col3) (:td nil ,col4))
-                   (push (make-log-row :time col1 :priority col2 :code col3 :message col4) this-run))
+                   (push (mk-modem-log-row (ensure-strings (list col1 col2 col3 col4))) this-run))
                  ((list* (satisfies keywordp) _ children)
                   (map nil #'recurse children)))))
       (recurse (closure-html:parse (drakma:http-request *modem-logs-uri* :connection-timeout 5) (closure-html:make-lhtml-builder)))
 
-      ;; NB: if we catch the modem during a reboot, it is possible that this webpage wasn't completly finished.
-      ;; If that happens, optima returns a proper list of HTML, which will cause us to crash later in fare-csv writes.
-      ;;
       (nreverse this-run))))
 
 (defun write-header-line (stream)
@@ -183,19 +188,19 @@
   (write-csv-line (append (list "Timestamp") *modem-status-headers* *modem-stats-headers*) stream))
 
 (defun write-table-line (stream &optional modem-status modem-stats)
-  "Get the modem status, then stats, and create/write a line to the CSV file."
+  "Write modem status, then stats to STREAM"
   (let (s)
     (push (get-timestamp-string) s)
 
     (if modem-status
         (dolist (e modem-status)
-          (push (table-row-value e) s))
+          (push (modem-status-row-value e) s))
         (dotimes (i (length *modem-status-headers*))
           (push 0 s)))
 
     (if modem-stats
         (dolist (e modem-stats)
-          (push (table-row-value e) s))
+          (push (modem-status-row-value e) s))
         (dotimes (i (length *modem-stats-headers*))
           (push 0 s)))
 
@@ -205,16 +210,16 @@
 ;; e.g., most recent error is (first *last-log*)
 (defparameter *last-log* nil "list of last log entries  read from http/modem.")
 
-(defun log-row-equal (a b)
+(defun modem-log-row-equal (a b)
   "Compare two modem log entries, ignoring the timestamp we add"
-  (and (string= (log-row-time a)     (log-row-time b))
-       (string= (log-row-priority a) (log-row-priority b))
-       (string= (log-row-code a)     (log-row-code b))
-       (string= (log-row-message a)  (log-row-message b))))
+  (and (string= (modem-log-row-time a)     (modem-log-row-time b))
+       (string= (modem-log-row-priority a) (modem-log-row-priority b))
+       (string= (modem-log-row-code a)     (modem-log-row-code b))
+       (string= (modem-log-row-message a)  (modem-log-row-message b))))
 
 (defun read-modem-log (f &optional (no-header t))
   "Read in the modem log and convert it back to a list of structs"
-  (let* ((ret (mapcar (lambda (l) (make-log-row-record l)) (fare-csv:read-csv-file f))))
+  (let* ((ret (mapcar (lambda (l) (mk-modem-log-row l)) (fare-csv:read-csv-file f))))
     (when no-header (pop ret))
     ret))
 
@@ -234,24 +239,24 @@ the timeframe from the last time we sampled."
       (write-csv-line '("Timestamp" "Real-time" "Time" "Priority" "Code" "Message") f)
       (dolist (l (nreverse log))
         (write-csv-line (list ts
-                              (log-row-time l)
-                              (log-row-priority l)
-                              (log-row-code l)
-                              (log-row-message l)) f))
+                              (modem-log-row-time l)
+                              (modem-log-row-priority l)
+                              (modem-log-row-code l)
+                              (modem-log-row-message l)) f))
       (setf *last-log* log)
       (return-from process-modem-log t))
 
     (let (new)
       (loop for row in log do
-        (when (log-row-equal row (first *last-log*))
+        (when (modem-log-row-equal row (first *last-log*))
           (return))
         (push row new))
       (dolist (row (nreverse new))
         (write-csv-line (list ts
-                              (log-row-time row)
-                              (log-row-priority row)
-                              (log-row-code row)
-                              (log-row-message row))
+                              (modem-log-row-time row)
+                              (modem-log-row-priority row)
+                              (modem-log-row-code row)
+                              (modem-log-row-message row))
                         f)))
     (setf *last-log* log)))
 
@@ -400,3 +405,30 @@ the timeframe from the last time we sampled."
               ((string="plot" msg)
                (do-plot))
               (t (setf file msg))))))) ; if not start/quit/plot, assume it's the name of the plot file to use
+
+(defparameter *up-time-pat*      "^(\\d+) days (\\d+)h:(\\d+)m:(\\d+)s")
+(defun uptime-seconds (v)
+  (optima:match v ((optima.ppcre:ppcre *up-time-pat* days hours mins secs)
+                   (+ (* (parse-integer days) 24 60 60)
+                      (* (parse-integer hours) 60 60)
+                      (* (parse-integer mins) 60)
+                      (parse-integer secs)))))
+(defun seconds-to-uptime (seconds)
+  (let* ((days (floor seconds (* 60 60 24)))
+         (seconds (rem seconds (* 60 60 24)))
+         (hours (floor seconds (* 60 60)))
+         (seconds (rem seconds (* 60 60)))
+         (minutes (floor seconds 60))
+         (seconds (rem seconds 60)))
+    (format nil "~d days ~dh:~dm:~ds" days hours minutes seconds)))
+
+(defun max-uptime (file)
+  (let ((times (mapcar (lambda (l) (mk-modem-stats-row l)) (fare-csv:read-csv-file file)))
+        (max 0)
+        (this-time))
+    (pop times)
+    (dolist (r times)
+      (setf this-time (uptime-seconds (modem-stats-row-system-up-time r)))
+      (if (and this-time (> this-time max))
+          (setf max this-time)))
+    (format t "Max uptime was ~a~%" (seconds-to-uptime max))))
