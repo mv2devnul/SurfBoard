c399c353b3a6dd35a30aea31bc72ae5b90a72b43
Mark VandenBrink <mark@vandenbrink.us>
2013-09-24 14:11:20 -0500

    adding packages

---
+++ packages.lisp
@@ -0,0 +1,6 @@
+;;; -*- Mode: Lisp;  show-trailing-whitespace: t; Base: 10; indent-tabs: nil; Syntax: ANSI-Common-Lisp; Package: CL-USER; -*-
+;;; Copyright (c) 2013, Mark VandenBrink. All rights reserved.
+(in-package #:cl-user)
+
+(defpackage #:surfboard
+  (:use #:common-lisp))
